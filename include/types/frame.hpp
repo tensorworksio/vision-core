f0dd1128861ded50328d56fbbbb6ce8b1c8b608e
denguir <anass.den@gmail.com>
2026-05-15 11:31:59 +0200

    frame type

--- include/types/frame.hpp
+++ include/types/frame.hpp
@@ -13,16 +13,17 @@ struct Frame
 {
     cv::Mat image;
     cv::Size size;
+    int type{CV_8UC3};
     TimePoint timestamp;
     int64_t id;
     std::vector<Detection> detections;
 
     static int64_t frame_counter;
 
-    Frame() : image(), size(0, 0), timestamp(std::chrono::system_clock::now()), id(frame_counter++) {}
+    Frame() : image(), size(0, 0), type(CV_8UC3), timestamp(std::chrono::system_clock::now()), id(frame_counter++) {}
 
     Frame(const cv::Mat &img, TimePoint ts = std::chrono::system_clock::now())
-        : image(img), size(img.size()), timestamp(ts), id(frame_counter++) {}
+        : image(img), size(img.size()), type(img.type()), timestamp(ts), id(frame_counter++) {}
 
     friend Frame &operator>>(cv::VideoCapture &cap, Frame &frame)
     {
@@ -32,6 +33,7 @@ struct Frame
         {
             frame.image = img;
             frame.size = img.size();
+            frame.type = img.type();
             frame.timestamp = std::chrono::system_clock::now();
             frame.id = frame_counter++;
         }
@@ -77,12 +79,13 @@ struct rfl::Reflector<Frame>
         int64_t id{0};
         std::chrono::system_clock::time_point timestamp{};
         cv::Size size{};
+        int type{CV_8UC3};
         std::vector<Detection> detections{};
     };
 
     static ReflType from(const Frame &f) noexcept
     {
-        return {f.id, f.timestamp, f.size, f.detections};
+        return {f.id, f.timestamp, f.size, f.type, f.detections};
     }
 
     static Frame to(const ReflType &r) noexcept
@@ -91,6 +94,7 @@ struct rfl::Reflector<Frame>
         f.id = r.id;
         f.timestamp = r.timestamp;
         f.size = r.size;
+        f.type = r.type;
         f.detections = r.detections;
         return f;
     }
